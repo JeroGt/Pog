104a14de7af6621afe38b19dd583981b600fcb0b
Jero <ericdusmail@gmail.com>
2022-05-22 22:21:23 +0200

    Initial commit

---
+++ java/de/jero/pog/Pog.java
@@ -0,0 +1,17 @@
+package de.jero.pog;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Pog extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
